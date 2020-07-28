c720a5f1d845319a3c29faf711aa6b6414837dcc
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-07-27 19:09:30 -0700

    test ping-pong

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ client.on('ready', () => {
     console.log(`Ready, logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+})
+
 client.login(process.env.BOT_TOKEN);
